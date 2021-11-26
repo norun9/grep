0a78fc5e476651c35e33e58b800827e195ebb16e
norun111 <u.t012807@gmail.com>
2021-11-26 22:07:05 +0900

    add

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,6 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
-
     Ok(())
 }
 
@@ -23,9 +21,48 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough argumanets");
         }
-        Ok(Config {
-            query: args[0].to_string(),
-            filename: args[1].to_string(),
-        })
+        let query;
+        let filename;
+        if args.len() < 2 {
+            query = args[0].to_string();
+            filename = args[1].to_string();
+        }
+        Ok(Config { query, filename })
+    }
+
+    fn get_info<'a>(args: &'a [String]) -> (&'a str, &'a str) {
+        let q = args[0].to_string();
+        let f = args[1].to_string();
+
+        (&q, &f)
+    }
+}
+
+// 'a: https://laysakura.github.io/2020/05/21/rust-static-lifetime-and-static-bounds/
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,11 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     let config = grep::Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = grep::run(config) {
         println!("Application error: {}", e);
